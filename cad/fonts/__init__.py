8d2b438dd4bd95da6499f7d5bc2d894bdf9fca31
Matti Eiden <snaipperi@gmail.com>
2023-07-08 13:50:36 +0300

    Clean up

--- cad/fonts/__init__.py
+++ cad/fonts/__init__.py
@@ -5,4 +5,3 @@ _base_path = os.path.dirname(__file__)
 blockschrift_font_path = os.path.join(_base_path, "blockschrift.ttf")
 panel_font_path = os.path.join(_base_path, "xA320PanelFont_V0.2b.ttf")
 fcu_font_path = os.path.join(_base_path, "xAirbusFCU.ttf")
-
